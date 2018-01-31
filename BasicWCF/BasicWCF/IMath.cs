943c72437fa56bd3287b16e73d07c015540fa66d
Billy Wu <lake_ice@hotmail.com>
2018-01-31 15:47:48 +0800

    MathService

---
+++ BasicWCF/BasicWCF/IMath.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+
+namespace BasicWCF
+{
+    [ServiceContract]
+    public interface IMath
+    {
+        [OperationContract]
+        double Add(double A, double B);
+
+        [OperationContract]
+        double Multiply(double A, double B);
+    }
+}
---
+++ BasicWCF/BasicWCF/MathService.cs
@@ -0,0 +1,15 @@
+﻿namespace BasicWCF
+{
+    public class MathService : IMath
+    {
+        public double Add(double A, double B)
+        {
+            return A + B;
+        }
+
+        public double Multiply(double A, double B)
+        {
+            return A * B;
+        }
+    }
+}
