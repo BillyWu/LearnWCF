cd4013f09759c38216046f94f6de35f9ce6c9440
Billy Wu <lake_ice@hotmail.com>
2018-02-01 17:38:27 +0800

    HostService

---
+++ BasicWCF/GettingStartedHostService/CalculatorService.cs
@@ -0,0 +1,30 @@
+﻿namespace Microsoft.ServiceModel.Samples
+{
+    public class CalculatorService : ICalculator
+    {
+        // Implement the ICalculator methods.
+        public double Add(double n1, double n2)
+        {
+            double result = n1 + n2;
+            return result;
+        }
+
+        public double Subtract(double n1, double n2)
+        {
+            double result = n1 - n2;
+            return result;
+        }
+
+        public double Multiply(double n1, double n2)
+        {
+            double result = n1 * n2;
+            return result;
+        }
+
+        public double Divide(double n1, double n2)
+        {
+            double result = n1 / n2;
+            return result;
+        }
+    }
+}
